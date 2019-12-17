15675990b43640b1cef95f7f0c9412ffdb5d5489
MinG822 <aestas822@gmail.com>
2019-12-17 23:07:07 +0900

    add vuetify plugin and apply navbar & footer

--- FRONTEND/src/main.js
+++ FRONTEND/src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import firebase from 'firebase'
+import vuetify from './plugins/vuetify';
 
 Vue.config.productionTip = false
 
@@ -23,5 +24,6 @@ firebase.initializeApp(firebaseConfig);
 
 new Vue({
   router,
+  vuetify,
   render: h => h(App)
 }).$mount('#app')
